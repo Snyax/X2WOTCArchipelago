97b9380409a1139ed3f7f01be6142c23eb0fe656
Tatu Leinonen <tatu.heikki.leinonen@gmail.com>
2026-06-28 22:14:46 +0300

    Implement Shadow Ops

---
+++ worlds/x2wotc/mods/shadow_ops_classes/__init__.py
@@ -0,0 +1,13 @@
+from .Items import items
+from .Locations import locations, soldier_classes, MAX_RANK
+
+name = "Shadow Ops Perk Pack"
+
+rule_priority = 0.0
+
+RANKS_STR = ", ".join(f"Ranks[{i-2}]={i}" for i in range(2, MAX_RANK+1))
+config: dict[str, str] = {
+    "WOTCArchipelago_Ranksanity": "".join(
+        f"+RanksanityData=(ID=\"{internal_name.title()}\", ClassName=\"{internal_name.title()}\", \\\\\n{RANKS_STR})\n" for internal_name in soldier_classes.keys()
+    )
+}
