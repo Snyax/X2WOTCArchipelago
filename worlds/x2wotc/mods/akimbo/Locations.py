1b251bb49e6cb4f71f191e34de39a6df6e2db97f
Tatu Leinonen <tatu.heikki.leinonen@gmail.com>
2026-06-28 21:55:33 +0300

    Implement Akimbo

---
+++ worlds/x2wotc/mods/akimbo/Locations.py
@@ -0,0 +1,21 @@
+from worlds.x2wotc.LocationData import (
+    X2WOTCLocationData,
+    get_new_location_id,
+    SOLDIER_RANK_LOCATION_PREFIX,
+    SOLDIER_RANK_LOCATION_INFIX,
+    DEFAULT_RANK_NAME_SET,
+)
+
+MAX_RANK = 7
+
+locations: dict[str, X2WOTCLocationData] = {
+    f"AkimboRank{rank}": X2WOTCLocationData(
+        display_name = SOLDIER_RANK_LOCATION_PREFIX + "Akimbo" + SOLDIER_RANK_LOCATION_INFIX + DEFAULT_RANK_NAME_SET[rank],
+        id = get_new_location_id(),
+        type = "SoldierRank",
+        tags = {"akimbo", f"item:AkimboRank:{rank - 1}"},
+        dlc = None,
+        normal_item = "AkimboRank"
+    )
+    for rank in range(2, MAX_RANK + 1)
+}
