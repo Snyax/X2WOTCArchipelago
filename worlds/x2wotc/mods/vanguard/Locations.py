a9fff9f9faec080caa61f21f42c1c7f55b899b1b
Tatu Leinonen <tatu.heikki.leinonen@gmail.com>
2026-06-29 12:08:03 +0300

    Implement Vanguard class

---
+++ worlds/x2wotc/mods/vanguard/Locations.py
@@ -0,0 +1,21 @@
+from worlds.x2wotc.LocationData import (
+    X2WOTCLocationData,
+    get_new_location_id,
+    SOLDIER_RANK_LOCATION_PREFIX,
+    SOLDIER_RANK_LOCATION_INFIX,
+    DEFAULT_RANK_NAME_SET,
+)
+
+MAX_RANK = 7
+
+locations: dict[str, X2WOTCLocationData] = {
+    f"AlphaRiflemanRank{rank}": X2WOTCLocationData(
+        display_name = SOLDIER_RANK_LOCATION_PREFIX + "Vanguard" + SOLDIER_RANK_LOCATION_INFIX + DEFAULT_RANK_NAME_SET[rank],
+        id = get_new_location_id(),
+        type = "SoldierRank",
+        tags = {"vanguard", f"item:AlphaRiflemanRank:{rank - 1}"},
+        dlc = None,
+        normal_item = "AlphaRiflemanRank"
+    )
+    for rank in range(2, MAX_RANK + 1)
+}
