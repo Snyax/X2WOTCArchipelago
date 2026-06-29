42f0130cd35ce32dc657b983ff6b522dd1df876e
Tatu Leinonen <tatu.heikki.leinonen@gmail.com>
2026-06-29 12:23:42 +0300

    Implement Commando class

---
+++ worlds/x2wotc/mods/commando/Locations.py
@@ -0,0 +1,21 @@
+from worlds.x2wotc.LocationData import (
+    X2WOTCLocationData,
+    get_new_location_id,
+    SOLDIER_RANK_LOCATION_PREFIX,
+    SOLDIER_RANK_LOCATION_INFIX,
+    DEFAULT_RANK_NAME_SET,
+)
+
+MAX_RANK = 7
+
+locations: dict[str, X2WOTCLocationData] = {
+    f"AlphaCommandoRank{rank}": X2WOTCLocationData(
+        display_name = SOLDIER_RANK_LOCATION_PREFIX + "Commando" + SOLDIER_RANK_LOCATION_INFIX + DEFAULT_RANK_NAME_SET[rank],
+        id = get_new_location_id(),
+        type = "SoldierRank",
+        tags = {"commando", f"item:AlphaCommandoRank:{rank - 1}"},
+        dlc = None,
+        normal_item = "AlphaCommandoRank"
+    )
+    for rank in range(2, MAX_RANK + 1)
+}
